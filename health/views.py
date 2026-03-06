1c7273484dbc2d0b94e40f95109e0e2bf32f43d9
Dmitry Lazenyuk <lazenyukdima@gmail.com>
2026-03-06 22:43:44 +0500

    Add disk health check

--- health/views.py
+++ health/views.py
@@ -3,6 +3,7 @@ import asyncio
 from dataclasses import asdict
 from django.http import JsonResponse
 from health_check import Database, Storage
+from health_check.contrib.psutil import Disk
 from health.decorators import health_check_timeout
 from health.security import health_check_auth
 from health.types import HealthStatus
@@ -13,6 +14,7 @@ START_TIME = time.time()
 async def health_check_custom(request):
     database_check = Database()
     storage_check = Storage()
+    disk_check = Disk()
 
     await database_check.get_result()
     await storage_check.get_result()
@@ -20,10 +22,11 @@ async def health_check_custom(request):
     check_results = await asyncio.gather(
         health_check_timeout(3)(database_check.get_result)(),
         health_check_timeout(5)(storage_check.get_result)(),
+        health_check_timeout(0.5)(disk_check.get_result)(),
     )
 
     is_healthy = all(res.status == HealthStatus.HEALTHY for res in check_results)
-    database_res, storage_res = check_results
+    database_res, storage_res, disk_res = check_results
 
     uptime_sec = int(time.time() - START_TIME)
     uptime_str = f"{uptime_sec // 3600}:{(uptime_sec % 3600) // 60:02d}:{uptime_sec % 60:02d}"
@@ -34,5 +37,6 @@ async def health_check_custom(request):
         "checks": {
             "db": asdict(database_res),
             "storage": asdict(storage_res),
+            "disk": asdict(disk_res),
         },
     })
